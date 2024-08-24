d54b8c40f35e6dc159de696fb950d3ac2ece26d6
Jeremi Biernacki <jeremi360@gmail.com>
2024-08-24 14:30:43 +0200

    fix search results display in IconFinder

--- addons/material-design-icons/icon_finder/icon_finder.gd
+++ addons/material-design-icons/icon_finder/icon_finder.gd
@@ -57,24 +57,29 @@ func update_icons_size(value: int):
 	icons_text.set("theme_override_font_sizes/normal_font_size", value)
 	update_table(search_line_edit.text)
 
-func update_table(filter:=""):
+func update_table(filter := ""):
 	var table = "[table={columns}, {inline_align}]"
+	var columns := int(size.x / size_slider.value) + 1
 	table = table.format({
-		"columns": int(size.x / size_slider.value),
+		"columns": columns,
 		"inline_align": INLINE_ALIGNMENT_CENTER
 	})
 
+	var cells := columns
 	for key in MaterialIconsDB.icons:
-		if filter:
-			if not (filter.to_lower() in key):
-				continue
-		
+		if filter: if not (filter.to_lower() in key): continue
+		cells -= 1
+		if cells <= 0: cells = columns
 		var link := "[url={link}]{text}[/url]"
 		var text := MaterialIconsDB.get_icon_char(key)
 		link = link.format({"link": key, "text": text})
 
 		var cell := "[cell]{link}[/cell]"
 		table += cell.format({"link": link})
+	
+	if cells > 0:
+		for c in cells:
+			table += "[cell] [/cell]"
 
 	table += "[/table]"
 	icons_text.parse_bbcode(table)
