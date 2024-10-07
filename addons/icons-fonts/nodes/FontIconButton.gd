56c1ff5e7ce2bb25c5600d050625bbffe752e636
Jeremi Biernacki <jeremi360@gmail.com>
2024-10-07 14:05:32 +0200

    try to crate new & better FontIconButton

--- addons/icons-fonts/nodes/FontIconButton.gd
+++ addons/icons-fonts/nodes/FontIconButton.gd
@@ -1,32 +1,47 @@
 @tool
-@icon("res://addons/icons-fontsnodes/FontIconButton.svg")
-extends Button
+@icon("res://addons/icons-fonts/nodes/FontIconButton.svg")
+extends HBoxContainer
 
 # todo add descreption and docs links when ready
 class_name FontIconButton
 
-@export_group("Icon", "icon_")
-## Name of Icon to display
-@export var icon_name := "image-outline":
+@export var icon_settings: FontIconSettings:
 	set(value):
-		icon_name = value
-		text = IconsFonts.get_icon_char(value)
-	
-	get: return icon_name
+		icon_settings = value
+		if !is_node_ready(): await ready
+		_font_icon.icon_settings = value
 
-@export_group("Icon", "icon_")
-## Size of the icon in range 16-128
-@export_range(16, 128, 1)
-var icon_size := 16:
+@export var text := "":
 	set(value):
-		icon_size = value
-		set("theme_override_font_sizes/font_size",value)
-	
-	get: return icon_size
+		text = value
+		if !is_node_ready(): await ready
+		_label.text = text
+
+@export var label_settings: LabelSettings:
+	set(value):
+		label_settings = value
+		if !is_node_ready(): await ready
+		_label.label_settings = label_settings
+
+@export_group("Styles", "style_")
+@export var style_normal: StyleBox:
+	set(value):
+		style_normal = value
+		add_theme_stylebox_override("normal", style_normal)
+
+var _font_icon: FontIcon
+var _label: Label
 
 func _ready():
-	clip_text = false
-	var font := IconsFonts
-	text = IconsFonts.get_icon_char(icon_name)
-	set("theme_override_fonts/font",font)
-	set("theme_override_font_sizes/font_size",icon_size)
+	_font_icon = FontIcon.new()
+	add_child(_font_icon)
+	_font_icon.add_theme_stylebox_override("normal", StyleBoxEmpty.new())
+
+	_label = Label.new()
+	add_child(_label)
+	_label.add_theme_stylebox_override("normal", StyleBoxEmpty.new())
+	
+	# todo make HBox to look 
+	style_normal = get_theme_stylebox("normal", "Button")
+	
+	# todo make HBox to behave like button
