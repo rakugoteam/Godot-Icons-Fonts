052d698a03025f056fe9ef757d8bbd1fe5b24364
Jeremi Biernacki <jeremi360@gmail.com>
2024-10-07 17:26:34 +0200

    make new FontIconButton that uses ButtonContainer as base

--- addons/icons-fonts/nodes/FontIconButton.gd
+++ addons/icons-fonts/nodes/FontIconButton.gd
@@ -1,11 +1,11 @@
 @tool
 @icon("res://addons/icons-fonts/nodes/FontIconButton.svg")
-extends HBoxContainer
+extends ButtonContainer
 
 # todo add descreption and docs links when ready
 class_name FontIconButton
 
-@export var icon_settings: FontIconSettings:
+@export var icon_settings := FontIconSettings.new():
 	set(value):
 		icon_settings = value
 		if !is_node_ready(): await ready
@@ -17,31 +17,38 @@ class_name FontIconButton
 		if !is_node_ready(): await ready
 		_label.text = text
 
-@export var label_settings: LabelSettings:
+@export var label_settings := LabelSettings.new():
 	set(value):
 		label_settings = value
 		if !is_node_ready(): await ready
 		_label.label_settings = label_settings
 
-@export_group("Styles", "style_")
-@export var style_normal: StyleBox:
+@export var button_margin := 5:
 	set(value):
-		style_normal = value
-		add_theme_stylebox_override("normal", style_normal)
+		button_margin = value
+		if !is_node_ready(): await ready
+		_margins.add_theme_constant_override("margin_top", button_margin)
+		_margins.add_theme_constant_override("margin_left", button_margin)
+		_margins.add_theme_constant_override("margin_bottom", button_margin)
+		_margins.add_theme_constant_override("margin_right", button_margin)
 
 var _font_icon: FontIcon
 var _label: Label
+var _box: BoxContainer
+var _margins: MarginContainer
 
 func _ready():
+	var empty_style := StyleBoxEmpty.new()
+	_box = BoxContainer.new()
 	_font_icon = FontIcon.new()
-	add_child(_font_icon)
-	_font_icon.add_theme_stylebox_override("normal", StyleBoxEmpty.new())
+	_font_icon.add_theme_stylebox_override("normal", empty_style)
+	_box.add_child(_font_icon)
 
 	_label = Label.new()
-	add_child(_label)
-	_label.add_theme_stylebox_override("normal", StyleBoxEmpty.new())
-	
-	# todo make HBox to look 
-	style_normal = get_theme_stylebox("normal", "Button")
-	
-	# todo make HBox to behave like button
+	_label.add_theme_stylebox_override("normal", empty_style)
+	_box.add_child(_label)
+
+	_margins = MarginContainer.new()
+	_margins.add_child(_box)
+
+	add_child(_margins)
