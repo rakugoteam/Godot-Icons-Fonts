8f8c3e23c13a78a8f552675704559f276622535c
Jeremi Biernacki <jeremi360@gmail.com>
2024-11-18 09:40:47 +0100

    make possible to move left-right icon in FontIconButton

--- addons/icons-fonts/nodes/FontIconButton.gd
+++ addons/icons-fonts/nodes/FontIconButton.gd
@@ -5,6 +5,14 @@
 class_name FontIconButton
 extends ButtonContainer
 
+@export_group("Icon", "icon_")
+@export var icon_on_right := true:
+	set(value):
+		icon_on_right = value
+		if !is_node_ready(): await ready
+		if value: _box.move_child(_font_icon, 1)
+		else: _box.move_child(_font_icon, 0)
+
 @export var icon_settings := FontIconSettings.new():
 	set(value):
 		icon_settings = value
@@ -18,7 +26,6 @@ extends ButtonContainer
 		if !is_node_ready(): await ready
 		_label.text = label_text
 
-@export_group("Label", "label_")
 @export var label_settings := LabelSettings.new():
 	set(value):
 		label_settings = value
@@ -29,10 +36,12 @@ extends ButtonContainer
 	set(value):
 		button_margin = value
 		if !is_node_ready(): await ready
-		_margins.add_theme_constant_override("margin_top", button_margin)
-		_margins.add_theme_constant_override("margin_left", button_margin)
-		_margins.add_theme_constant_override("margin_bottom", button_margin)
-		_margins.add_theme_constant_override("margin_right", button_margin)
+		var margins := [
+			"margin_top", "margin_left",
+			"margin_bottom", "margin_right"
+		]
+		for margin in margins:
+			_margins.add_theme_constant_override(margin, button_margin)
 
 var _font_icon: FontIcon
 var _label: Label
@@ -50,6 +59,8 @@ func _ready():
 	_label = Label.new()
 	_label.add_theme_stylebox_override("normal", empty_style)
 	_box.add_child(_label)
+
+	self.icon_on_right = icon_on_right
 
 	_margins = MarginContainer.new()
 	_margins.add_child(_box)
