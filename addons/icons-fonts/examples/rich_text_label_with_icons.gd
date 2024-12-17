57ab57df6f80472de59e0f7ecccb6f6aae04bb5d
Jeremi Biernacki <jeremi360@gmail.com>
2024-12-17 12:47:25 +0100

    fix overflow with warnings in RichTextLabel example

--- addons/icons-fonts/examples/rich_text_label_with_icons.gd
+++ addons/icons-fonts/examples/rich_text_label_with_icons.gd
@@ -4,6 +4,7 @@ extends RichTextLabel
 @export_multiline
 var text_with_icons: String:
 	set(value):
+		if !is_node_ready(): await ready
 		text_with_icons = value
 		bbcode_enabled = true
 		text = IconsFonts.parse_text(value)
